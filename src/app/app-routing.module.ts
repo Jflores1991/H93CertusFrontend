e1fe4f23f2564c9cb854e852aeca6a28818fd643
John Doe <jflorese@certus.edu.pe>
2022-11-30 19:33:19 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { ListarclientesComponent } from './pages/listarclientes/listarclientes.component';
+import { ModuloadminitrativoComponent } from './pages/moduloadminitrativo/moduloadminitrativo.component';
+import { BienvenidoComponent } from './pages/admin/bienvenido/bienvenido.component';
+import { SliderbarComponent } from './pages/admin/sliderbar/sliderbar.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', component : HomeComponent,pathMatch : 'full'},
+  {path : 'login', component : LoginComponent,pathMatch : 'full'},
+  {path : 'signup', component : SignupComponent,pathMatch : 'full'},
+  {path : 'listarClientes', component : ListarclientesComponent,pathMatch : 'full'},
+  {path : 'moduloAdmin', component : ModuloadminitrativoComponent,pathMatch : 'full'},
+  {path : 'profile', component : ProfileComponent,pathMatch : 'full'},
+
+  {path : 'admin' ,component : DashboardComponent, 
+  children:[
+    {
+      path : '' ,component : BienvenidoComponent, 
+    },
+    {
+      path : 'sliderbar' ,component : SliderbarComponent, 
+    }
+   ] }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,67 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {FormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import { HomeComponent } from './pages/home/home.component';
+import {MatIconModule} from '@angular/material/icon';
+import { ListarclientesComponent } from './pages/listarclientes/listarclientes.component';
+import { ModuloadminitrativoComponent } from './pages/moduloadminitrativo/moduloadminitrativo.component';
+import { BienvenidoComponent } from './pages/admin/bienvenido/bienvenido.component';
+import {MatDividerModule} from '@angular/material/divider';
+import { SliderbarComponent } from './pages/admin/sliderbar/sliderbar.component';
+import {MatListModule} from '@angular/material/list';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent,
+    ListarclientesComponent,
+    ModuloadminitrativoComponent,
+    BienvenidoComponent,
+    SliderbarComponent,
+    DashboardComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    FormsModule,
+    MatCardModule,
+    MatIconModule,
+    MatDividerModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
